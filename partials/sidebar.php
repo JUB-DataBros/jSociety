5a152f103b77263f6b57b0cf3c06e541bc19f1f6
Baris Demirel <bdemirel732@gmail.com>
2017-10-22 17:00:03 +0200

    Sidebar and Disclaimer html

---
+++ partials/sidebar.php
@@ -0,0 +1,6 @@
+<div class="sidebar">
+  <input type="text" name="search">
+  <button type="button" class="side-button" name="home">Homepage</button>
+  <button type="button" class="side-button" name="profile">Profile</button>
+  <button type="button" class="side-button bottom" name="Disclaimer">Disclaimer</button>
+</div>
