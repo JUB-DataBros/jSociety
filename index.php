a1a7ef4c217618c1d63b12179416317a742d6b6d
Alkim Akgun <25912627+alkim-akgun@users.noreply.github.com>
2017-10-22 20:25:07 +0200

    22/10/2017 -alkim

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+  <title>jSociety</title>
+</head>
+<body>
+<?php
+  include('partials/header.php');
+  include('partials/sidebar.php');
+?>
+<div class="body">
+</div>
+<script src="js/jquery.js"></script>
+<script src="js/main.js"></script>
+<script>
+getPage = findGetParameter("page");
+switch(getPage){
+  case "feed":
+  case "profile":
+  case "events":
+  case "clubs":
+  case "settings":
+    loadPage(getPage + ".php");
+    break;
+}
+</script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,29 @@
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+function loadPage(page) {
+  $.ajax({
+    method: "POST",
+    url: page,
+    data: {}
+  })
+
+  .done(function(data) {
+    $(".body").append(page + ": " + data);
+  })
+
+  .fail(function(data) {
+    $(".body").append("AJAX Request to page " + page + " failed: <br>");
+    $(".body").append(data);
+  });
+}
