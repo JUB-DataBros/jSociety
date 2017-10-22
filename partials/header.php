a61de9c78cd2d5edf1feb3e4133e4c0ace2086d8
Alkim Akgun <25912627+alkim-akgun@users.noreply.github.com>
2017-10-22 18:14:02 +0200

    header.php comment added

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,7 @@
 <div class="header">
   <a href="index.php">
     <img class="logo" src="images/logo.png" alt="jSociety">
+    <!-- Logo does not load on header.php but works on index.php -->
   </a>
   <h1>jSociety</h1>
 </div>
