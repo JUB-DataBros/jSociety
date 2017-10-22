d06d1723b0d3ddb42922f21bce04522b5300ec70
Baris Demirel <bdemirel732@gmail.com>
2017-10-22 17:50:33 +0200

    Corrected header for index

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 <div class="header">
-  <a href="/index.php">
-    <img class="logo" src="../images/logo.png" alt="jSociety">
+  <a href="index.php">
+    <img class="logo" src="images/logo.png" alt="jSociety">
   </a>
   <h1>jSociety</h1>
 </div>
