b5fdd4662b639b57109e71ad94e15ffa60a5e75a
Baris Demirel <bdemirel732@gmail.com>
2017-10-25 22:55:37 +0200

    Design Step 1

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Link to the page:
 http://clabsql.clamv.jacobs-university.de/~aakgun/index.html
 
 For Filezilla, use sftp!
+
+Email address:
+jub.databros@gmail.com
--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
-  <input type="text" name="search">
-  <button type="button" class="side-button" name="home">Homepage</button>
-  <button type="button" class="side-button" name="profile">Profile</button>
+  <input type="text" name="search" placeholder="Search"><button type="button" id="search"><img src="images/search.png"></button><br>
+  <button type="button" class="side-button" name="home">Homepage</button><br>
+  <button type="button" class="side-button" name="profile">Profile</button><br>
   <button type="button" class="side-button bottom" name="Disclaimer">Disclaimer</button>
 </div>
