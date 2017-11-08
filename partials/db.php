8cc63a85f4085a7965529b7af76c0f762d7f4a25
Alkim Akgun <25912627+alkim-akgun@users.noreply.github.com>
2017-11-08 23:34:06 +0100

    edited db.php

--- partials/db.php
+++ partials/db.php
@@ -7,22 +7,11 @@
   if (!$conn) {
     die("<h1>Database connection failed</h1><br>Error:<br>" . mysql_error());
   }
-  mysql_select_db($dbName) or die(mysql_error());
+  mysql_select_db($dbName) or die("<h1>Database selection failed</h1><br>Error:<br>" . mysql_error());
   mysql_set_charset('utf8');
 
 
   function runSQL($sql) {
-    if($_SESSION['type'] == 0) {
-      $logTable = "JSO_ADMINLOG";
-    }
-    elseif($_SESSION['type'] == 1) {
-      $logTable = "JSO_ADMINLOG";
-    }
-    else {
-      die("<h1>Invalid SQL Query</h1><br>Invalid type in runsql(" . $sql . ", " . $_SESSION['type'] . ")");
-      return false;
-    }
-
     $query = mysql_query($sql);
 
     if(!$query) {
@@ -35,7 +24,7 @@
   }
 
   function log($action) {
-    if($_SESSION['type']== 0) { //ADMINLOG --> username
+    if($_SESSION['type'] == 0) { //ADMINLOG --> username
       $sql = "INSERT INTO JSO_ADMINLOG VALUES(USERNAME = '" . $_SESSION['username'] ."', ACTION = '" . $action . "')";
       mysql_query($sql); //Cannot handle error here
     }
@@ -48,8 +37,4 @@
       return 0;
     }
 
-
-
-
-  }
 ?>
