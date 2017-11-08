d0e4d25a1b61c6a40463f51581d1c253cbb18a55
Baris Demirel <bdemirel732@gmail.com>
2017-11-08 23:35:24 +0100

    Create event page complete, needs code for saving

--- routes/createevent.php
+++ routes/createevent.php
@@ -1,21 +1,19 @@
 <div>
 Name: <input type="text" name="ename"><br>
-Club: <select name="eclub"></select><br>
+Club: <select name="eclub">
+<?php
+include("partials/db.php");
+
+$stmt = "SELECT c.id c.name FROM JSO_CLUB c WHERE c.organisor = {$_SESSION['id']}";
+$query = runSQL($stmt);
+
+while($result = mysql_fetch_array($query)){
+  echo "<option value=\"{$result[0]}\">{$result[1]}</option>
+  ";
+}
+?></select><br>
 Place: <input type="text" name="eplace"><br>
 Starts at: <input type="time" name="estart"> Ends at: <input type="time" name="eend"><br>
 Detail: <textarea name="edetail" rows="3"></textarea><br>
 <input type="submit" name="create_event_detail" value="Create">
 </div>
-
-<?php
-  if(isset($_POST['create_event_detail'])) {
-    $sql = "INSERT INTO dabr_events VALUES(name='" . mysqli_real_escape_string($_POST['create_event_name']) . "', createdby='" . $_SESSION['username'] . "', detail='" . mysql_real_escape_string($_POST['create_event_detail']) . "')";
-    $rs = $conn->query($sql);
-    if($rs === false) {
-      trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-    }
-    else {
-      echo "Event successfully created";
-    }
-  }
-?>
