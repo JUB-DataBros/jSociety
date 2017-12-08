f9da7bea373c484bafaa59cc501187c246625cbd
Alkim Akgun <25912627+alkim-akgun@users.noreply.github.com>
2017-12-08 11:00:50 +0100

    added sample data

--- routes/clubs.php
+++ routes/clubs.php
@@ -1,26 +1,60 @@
 Display clubs:
-<select id="selector" onChange="loadPage('routes/clubs.php?show=' + $('#selector').val())">
-	<option value='def'>Interested</option>
-	<option value='all' <?php $show = $_GET['show'] ? $_GET['show'] : "def"; if($show=="all") echo "selected";?>>All</option>
+<select id="selector">
+	<option value="select" selected>Select</option>
+	<option value='all' <?php echo ($_GET["show"] == "all" ? "selected" : "");?>>All</option>
+	<option value='interested'<?php echo ($_GET["show"] == "interested" ? "selected" : "");?>>Interested</option>
 </select>
+<script>
+	$('#selector').change(function(){
+		if($(this).val() != "select") {
+			//updateGet("show", $(this).val());
+			loadPage("routes/clubs.php?show=" + $(this).val());
+		}
+	});
+	//$('#selector').val('all').trigger('change');
+</script>
+<!--
+<input type="radio" name="queryclubs" value='int' id="queryclubsint" <?php //echo ($_SESSION['show'] ? "selected='selected" : "");?> >Interested
+<input type="radio" name="queryclubs" value='all' id="queryclubsall">All
+<script>
+	document.ready(function() {
+		$("#queryclubsint").on('click', function(){
+				loadPage('routes/clubs.php?show=int')
+		})
+		$("#queryclubsall").on('click', function(){
+		    loadPage('routes/clubs.php?show=all')
+		})
+	});
+</script>
+-->
 <table>
 <?php
+	//echo "GET show: " . $_GET['show'];
 	include_once("../essentials/db.php");
 	include_once("../essentials/security_check.php");
 	if ($_GET['show'] == "all") {
-		$sql = "SELECT c.ID, c.NAME, s.NAME, h.NAME FROM JSO_CLUB AS c, JSO_STUDENT AS s, JSO_HOBBY as h, JSO_STUDENT_HOBBY AS sh WHERE c.APPROVED = 1 AND c.ORGANISOR = s.ID AND c.FOCUS = h.ID AND h.ID = sh.HOBBYID";
+		/*
+		$sql = "SELECT c.NAME as NAME, s.NAME AS CREATEDBY, h.NAME AS FOCUS FROM JSO_CLUB AS c, JSO_STUDENT AS s, JSO_HOBBY as h, JSO_STUDENT_HOBBY AS sh WHERE c.APPROVED = 1 AND c.ORGANISOR = s.ID AND c.FOCUS = h.ID";
 		//SQL code is incorrect. JSO_HOBBY does not have subhobbies
 		$result = runSQL($sql, array());
 		if($result == null) {
 			die("<h1 style='color:red'>Error in database querying</h1> <br>Error Code: Q1005");
 		}
 		$clubs = $result -> fetchAll();
-		for ($i=0; $i<count($clubs); $i+=2) {
-			echo "<tr><th>{$clubs[$i]['c.NAME']}</th><th>{$clubs[$i+1]['c.NAME']}</th></tr>
-						<tr><td>{$clubs[$i]['h.NAME']}</td><td>{$clubs[$i+1]['h.NAME']}</td></tr>
-						<tr><td>Organized by: {$clubs[$i]['s.NAME']}</td><td> Organized by: {$clubs[$i+1]['s.NAME']}</td></tr>";
+
+		echo "<table><tr><th>Club Name</th><th>Created By</th><th><Focus></th></tr>";
+		$i = 0;
+		while($i < count($result)){ //
+			//$row = $result[i];
+			//echo $result[i][0] . "<br>";
+			echo "<tr><td>" . $result[$i]['NAME'] . "</td><td>" . $result[$i]['CREATEDBY'] . "</td><td>" . $result[$i]['FOCUS'] . "</td></tr>";
+			$i = $i + 1; //
 		}
-	} else {
+		echo "</table>";
+		*/
+		die("<h1 style='color:purple'>This page is under construction</h1>");
+	} elseif($_GET['show'] == "interested") {
+		/*
 		$sql = "SELECT c.ID, c.NAME, s.NAME, h.NAME FROM JSO_CLUB AS c, JSO_STUDENT AS s WHERE c.APPROVED = 1 AND c.ORGANISOR = s.ID AND c.FOCUS = h.ID AND c.FOCUS IN (SELECT HOBBYID FROM JSO_STUDENT_HOBBY AS h WHERE STUDENTID = :studentid)";
 		$args = array(":studentid" => $_SESSION['studentid']);
 		$result = runSQL($sql, $args);
@@ -33,6 +67,8 @@ Display clubs:
 						<tr><td>{$clubs[$i]['h.NAME']}</td><td>{$clubs[$i+1]['h.NAME']}</td></tr>
 						<tr><td>Organized by: {$clubs[$i]['s.NAME']}</td><td> Organized by: {$clubs[$i+1]['s.NAME']}</td></tr>";
 		}
+		*/
+		die("<h1 style='color:purple'>This page is under construction</h1>");
 	}
 
 ?>
--- routes/events.php
+++ routes/events.php
@@ -1,8 +1,13 @@
 <?php
 include_once("../essentials/db.php");
 include_once("../essentials/security_check.php");
-  //$sql = "SELECT name, createdby, detail FROM dabr_events";
-  //$result = runSQL($sql);
+  $sql = "SELECT NAME, ORGANIZEDBY, DETAIL, STARTTIME, ENDTIME FROM JSO_EVENT WHERE APPROVED = 1";
+  $result = runSQL($sql, Array());
+  if($result == null) {
+    die("<h1 style='color:red'>Error in database querying</h1> <br>Error Code: Q1007");
+  }
+  $result = $result -> fetchAll();
+  /*
   $result[0]['name'] = "Chess Society Meeting"; //
   $result[0]['createdby'] = "John Doe"; //
   $result[0]['detail'] = "Come to Mercator common room"; //
@@ -12,13 +17,13 @@ include_once("../essentials/security_check.php");
   $result[2]['name'] = "Waffle Thursday"; //
   $result[2]['createdby'] = "Arya Stark"; //
   $result[2]['detail'] = "Order your waffles on facebook"; //
+*/
 
-  echo "<table><tr><th>Event Name</th><th>Created By</th><th><Details></th></tr>";
-  //while($row = mysql_fetch_assoc($result)){
+  echo "<table style='border-spacing:50px'><tr><th>Event Name</th><th>Created By</th><th>Details</th><th>Begins</th><th>Ends</th></tr>";
   $i = 0; //
-  while($i < 3){ //
+  while($i < count($result)){ //
     //$row = $result[i];
-    echo "<tr><td>" . $result[$i]['name'] . "</td><td>" . $result[$i]['createdby'] . "</td><td>" . $result[$i]['detail'] . "</td></tr>"; //
+    echo "<tr><td>" . $result[$i]['NAME'] . "</td><td>" . $result[$i]['ORGANIZEDBY'] . "</td><td>" . $result[$i]['DETAIL'] . "</td><td>" . $result[$i]['STARTTIME'] . "</td><td>" . $result[$i]['ENDTIME'] . "</td></tr>"; //
     $i = $i + 1; //
   }
   echo "</table>";
